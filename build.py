35b3b81dfc7cc9f58c686d622366e38a3fc6c8bc
Ved Nig <64083352+vednig@users.noreply.github.com>
2022-01-03 06:36:11 +0530

    Update build.py

--- build.py
+++ build.py
@@ -7,8 +7,11 @@ path = r"md/"
 # call listdir() method
 # path is a directory of which you want to list
 directories = os.listdir( path )
-
+mdlist=[]
 # This would print all the files and directories
 for file in directories:
    print("Recd. "+file)
+   mdlist.append(file)
+  
+print(mdlist)
     
