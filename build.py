a80a874fe5cbed90718cdfa51d174e8468639944
Ved Nig <64083352+vednig@users.noreply.github.com>
2022-01-03 06:32:35 +0530

    Create build.py

---
+++ build.py
@@ -0,0 +1,14 @@
+
+import os
+
+# specify your path of directory
+path = r"md/"
+
+# call listdir() method
+# path is a directory of which you want to list
+directories = os.listdir( path )
+
+# This would print all the files and directories
+for file in directories:
+   print("Recd. "+file)
+    
